690fe036d26031aec90a6b2347eac6157c430bd3
Daco2020 <dacokim32@gmail.com>
2023-09-30 00:35:02 +0900

    feat: 수익화 기능 추가

--- main.py
+++ main.py
@@ -14,13 +14,17 @@ import streamlit as st
 import tempfile
 import os
 
-from dotenv import load_dotenv
+from streamlit_extras.buy_me_a_coffee import button
 
-load_dotenv()
-
+# 후원 버튼
+button(username="daco2020", floating=True, width=221)
 
 # Title
 st.title("ChatPDF")
+st.write("---")
+
+# openai api key input
+openai_key = st.text_input("OpenAI API Key", type="password")
 st.write("---")
 
 # File Uploader
@@ -53,13 +57,14 @@ if uploaded_file is not None:
     docs = text_splitter.split_documents(pages)
 
     # Embeddings
-    embeddings_model = OpenAIEmbeddings()
+    embeddings_model = OpenAIEmbeddings(openai_api_key=openai_key)
 
     # load in to chroma
     # 가공된 문서를 임베딩 모델로 저장
     # 임베딩은 정보를 가져오는 역할
     # 생성은 llm 의 역할
-    db = Chroma.from_documents(docs, embeddings_model, persist_directory="chroma_db")
+    # db = Chroma.from_documents(docs, embeddings_model, persist_directory="chroma_db")
+    db = Chroma.from_documents(docs, embeddings_model)
 
     # Question
     # q = "What is the meaning of life?"
@@ -75,8 +80,12 @@ if uploaded_file is not None:
 
     if st.button("전송"):
         with st.spinner("잠시만 기다려주세요..."):
-            llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=1, streaming=True)
+            llm = ChatOpenAI(
+                openai_api_key=openai_key,
+                model_name="gpt-3.5-turbo",
+                temperature=0.5,
+                streaming=True,
+            )
             qa_chain = RetrievalQA.from_chain_type(llm, retriever=db.as_retriever())
             result = qa_chain.run(query=q)
-            print(result)
             st.write(result)
